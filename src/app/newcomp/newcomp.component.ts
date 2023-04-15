6727eba7e3c53313220fa44cf59093b67309b488
Ajay-Gaikwad07 <130345085+Ajay-Gaikwad07@users.noreply.github.com>
2023-04-15 16:56:38 +0530

    Add files via upload

---
+++ src/app/newcomp/newcomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newcomp',
+  templateUrl: './newcomp.component.html',
+  styleUrls: ['./newcomp.component.css']
+})
+export class NewcompComponent implements OnInit {
+public Message="Marvellous Infosystem"
+ public Name ="Marvellous"
+public Nameof="Infosystem"
+  ngOnInit(): void {
+  }
+
+}
